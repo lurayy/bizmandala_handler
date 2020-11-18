3b94ed4789f2148f516a0f4d47ebb511f43c475a
Kabir S. Tamari <kabirtamari@gmail.com>
2020-11-18 15:38:07 +0545

    script changed

--- erp/cron.py
+++ erp/cron.py
@@ -1,18 +1,18 @@
 from erp.models import ERP
 from commerce.models import Credit, Setting
 from erp.models import TestModel
-
+import datetime
 def handle_credits():
-    TestModel.objects.create(
-        name = 'hello'
-    )
     try:
         settings = Setting.objects.all()[0]
     except:
-        raise Exception("Setting are not yet set. Please define setting from the admin panel.")
+        print("Setting are not yet set. Please define setting from the admin panel.")
     for erp in ERP.objects.filter(is_running = True):
-        if erp.credit.left_days <= -settings.lowest_purchase_time_limit_in_days:
-            erp.stop_container()
-        else:
-            erp.credit.left_days = erp.credit.left_days - 1
-            erp.credit.save()
+        try:
+            if erp.credit.left_days <= -settings.lowest_purchase_time_limit_in_days:
+                erp.stop_container()
+            else:
+                erp.credit.left_days = erp.credit.left_days - 1
+                erp.credit.save()
+        except Exception as exp:
+            print(exp, erp.id)
