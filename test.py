397a5640895f93c86354b85833fa8a6053d3b75a
Kabir S. Tamari <kabirtamari@gmail.com>
2020-11-24 10:25:19 +0545

    sycn

--- test.py
+++ test.py
@@ -1,64 +1,83 @@
 # import requests
 # from erp_handler.settings import docker_ip, docker_port, docker_protocol
-# from django.utils.timezone import now
-# import dateutil.parser
-# from datetime import datetime, timezone
+# # from django.utils.timezone import now
+# # import dateutil.parser
+# # from datetime import datetime, timezone
 
-# print(docker_ip)
+# # print(docker_ip)
 
-# url = '{}://{}:{}/containers/{}/json'.format(docker_protocol, docker_ip, docker_port, '0b03b5a26ffe')
+# url = '{}://{}:{}/containers/json'.format(docker_protocol, docker_ip, docker_port)
 # s = requests.get(url)
 # s = s.json()
-# print(s['State'])
-# print(s['State']['StartedAt'])
-# print(s['State']['FinishedAt'])
+# # print(s['State'])
+# # print(s['State']['StartedAt'])
+# # print(s['State']['FinishedAt'])
 
-# now = datetime.now(timezone.utc)
+# # now = datetime.now(timezone.utc)
 
-# start = dateutil.parser.parse(s['State']['StartedAt'])
+# # start = dateutil.parser.parse(s['State']['StartedAt'])
 
-# end = dateutil.parser.parse(s['State']['FinishedAt'])
+# # end = dateutil.parser.parse(s['State']['FinishedAt'])
 
-# if start > end:
-#     print('running')
+# # if start > end:
+# #     print('running')
 
 
-# # print(start - end), data=json.dumps(data)
+# # # print(start - end), data=json.dumps(data)
 
-# print(now - start)
+# # print(now - start)
 
-import requests
-import json
+# # import requests
+# # import json
 
-base_url = "http://localhost:8000/api/v1/"
+# # base_url = "http://localhost:8000/api/v1/"
 
-def login():    
-    creds = {
-        "username":"admin",
-        "password":"pass"
-    }
-    log_in_url = base_url+"user/"
+# # def login():    
+# #     creds = {
+# #         "username":"admin",
+# #         "password":"pass"
+# #     }
+# #     log_in_url = base_url+"user/"
 
-    r = requests.post(log_in_url, data= json.dumps(creds))
-    print(r.json())
-    token = r.json()["token"]
-    print(token)
+# #     r = requests.post(log_in_url, data= json.dumps(creds))
+# #     print(r.json())
+# #     token = r.json()["token"]
+# #     print(token)
 
-    headers = {
-        "authorization":token,
-    }
-    sess = requests.get(base_url+"user", headers=headers)
-    print(sess.text)
-    return headers
+# #     headers = {
+# #         "authorization":token,
+# #     }
+# #     sess = requests.get(base_url+"user", headers=headers)
+# #     print(sess.text)
+# #     return headers
 
-def call(headers, data, url):
-    sess = requests.get(base_url+url, headers=headers)
-    print(sess.text)
+# # def call(headers, data, url):
+# #     sess = requests.get(base_url+url, headers=headers)
+# #     print(sess.text)
 
-if __name__ == "__main__":
-    headers = login()
+# # if __name__ == "__main__":
+# #     headers = login()
     
-    # ------------------------------------------- Data Here ----------------------------------------
-    data = {}
-    url = 'user'
-    call(headers, data, url)
+# #     # ------------------------------------------- Data Here ----------------------------------------
+# #     data = {}
+# #     url = 'user'
+# #     call(headers, data, url)
+import os
+
+id = 3
+erp_image = 'erp_web_base'
+db_image = 'erp_db_base'
+network_name = "erp_net_"+str(id)
+db_name = "db_"+str(id)
+erp_name = "erp_"+str(id)
+cmd = 'docker network create --attachable {}'.format(network_name)
+os.system(cmd)
+cmd = 'docker run --name {0} --label {0} --network {1} --network-alias=db --hostname db -d {2}'.format(
+    db_name, network_name, db_image)
+os.system(cmd)
+cmd = 'docker run -d --name {0} --label {0} --network {1} --hostname {2} --link {3} {2}'.format(
+    erp_name, network_name, erp_image, db_name)
+os.system(cmd)
+cmd = 'docker exec {} /bin/bash -c "/usr/src/app/seeder.sh"'.format(
+    erp_name)
+os.system(cmd)
